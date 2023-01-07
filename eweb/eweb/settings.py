0f3a02486e2bc160323bf4c67687d4b84307c4fa
Vishu <vs1015242@gmail.com>
2023-01-07 11:24:35 +0530

    fix some issue

--- eweb/eweb/settings.py
+++ eweb/eweb/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i20xowpw-c(d!j*_r#z98aqasx3=z7jn0+$fijh&11l&@6y1#('
+SECRET_KEY = 'MY SERECT KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
