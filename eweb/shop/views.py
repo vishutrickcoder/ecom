fc77e75c25ba83d81b279f391f6f9ae385235dca
Vishu <vs1015242@gmail.com>
2023-01-09 10:41:49 +0530

    Adding Pagination

--- eweb/shop/views.py
+++ eweb/shop/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
 from .models import Product
+from django.core.paginator import Paginator
 # Create your views here.
 
 def index(request):
     product_objects = Product.objects.all()
 
+    # search func
     item_name = request.GET.get("item_name")
     if item_name != '' and item_name is not None:
         product_objects = product_objects.filter(title__icontains=item_name)
+
+    # pagination
+    paginator = Paginator(product_objects,8)
+    page = request.GET.get('page')
+    product_objects = paginator.get_page(page)
+
     return render(request,"shop/index.html" , {"product_objects" : product_objects})
