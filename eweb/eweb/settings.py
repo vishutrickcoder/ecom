4e356bfaf4a151b2861b82f8975521986646304b
Vishu <vs1015242@gmail.com>
2023-01-17 16:37:23 +0530

    some configrations

--- eweb/eweb/settings.py
+++ eweb/eweb/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'MY SERECT KEY'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -125,7 +124,3 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-import os
-
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
