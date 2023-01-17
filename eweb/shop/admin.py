f0b3b78505d98921cd86f3d564c7b6429f3df4c4
Vishu <vs1015242@gmail.com>
2023-01-17 16:01:50 +0530

    Custumize Admin and improve Ui for Better Exprience

--- eweb/shop/admin.py
+++ eweb/shop/admin.py
@@ -2,5 +2,19 @@ from django.contrib import admin
 from .models import Product,Order
 # Register your models here.
 
-admin.site.register(Product)
+admin.site.site_header = "E-Commerse Web App"
+admin.site.site_title = "E-comm Web"
+admin.site.index_title = "Managing E-com Orders and Products"
+
+class ProductsAdmin(admin.ModelAdmin):
+
+    def change_default(self,request,queryset):
+        queryset.update(category="default")
+
+    list_display = ("title","price","discount_price","category","description")
+    search_fields = ("title","category")
+    actions = ("change_default",)
+    list_editable = ("price","category")
+
+admin.site.register(Product,ProductsAdmin)
 admin.site.register(Order)
